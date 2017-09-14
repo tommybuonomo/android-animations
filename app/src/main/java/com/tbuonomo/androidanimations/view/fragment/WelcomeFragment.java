60cf824c76c58d542c8968c982f29f6a835356b6
tbuonomo <tbuonomo@ebiz.fr>
2017-09-14 14:06:18 +0200

    Change welcome fragment

--- app/src/main/java/com/tbuonomo/androidanimations/view/fragment/WelcomeFragment.java
+++ app/src/main/java/com/tbuonomo/androidanimations/view/fragment/WelcomeFragment.java
@@ -3,13 +3,15 @@ package com.tbuonomo.androidanimations.view.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import com.tbuonomo.androidanimations.R;
-import com.tbuonomo.materialsquareloading.MaterialSquareLoading;
 
 /**
  * Created by tommy on 03/09/17.
@@ -17,7 +19,9 @@ import com.tbuonomo.materialsquareloading.MaterialSquareLoading;
 
 public class WelcomeFragment extends Fragment {
 
-  @BindView(R.id.welcome_square_loading_view) MaterialSquareLoading materialSquareLoading;
+  @BindView(R.id.welcome_frame_layout) FrameLayout welcomeFrameLayout;
+
+  private boolean layoutInitialized;
 
   @Nullable @Override public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
     View rootView = inflater.inflate(R.layout.fragment_welcome, container, false);
@@ -26,7 +30,34 @@ public class WelcomeFragment extends Fragment {
   }
 
   @Override public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-    materialSquareLoading.show();
+    setUpFrameLayoutAnimation();
   }
 
+  private void setUpFrameLayoutAnimation() {
+    welcomeFrameLayout.getViewTreeObserver().addOnGlobalLayoutListener(() -> {
+      if (layoutInitialized) {
+        return;
+      }
+
+      int verticalCount = 6;
+      int horizontalCount = 10;
+      int cardWidth = welcomeFrameLayout.getWidth() / verticalCount;
+      int cardHeight = welcomeFrameLayout.getHeight() / horizontalCount;
+
+      for (int i = 0; i < verticalCount; i++) {
+        for (int j = 0; j < horizontalCount; j++) {
+          View item = LayoutInflater.from(getContext()).inflate(R.layout.item_welcome_card, welcomeFrameLayout, false);
+          FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams) item.getLayoutParams();
+          layoutParams.width = cardWidth;
+          layoutParams.height = cardHeight;
+          layoutParams.topMargin = j * cardHeight;
+          layoutParams.leftMargin = i * cardWidth;
+          CardView cardView = item.findViewById(R.id.item_welcome_card_view);
+          cardView.setCardBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorPrimaryDark));
+          welcomeFrameLayout.addView(item);
+        }
+      }
+      layoutInitialized = true;
+    });
+  }
 }
